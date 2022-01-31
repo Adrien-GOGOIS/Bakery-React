72145ee041906fbedfe80ef2db27fdfc496ae9d4
Adrien GOGOIS <ad.gogois@gmail.com>
2022-01-31 11:53:49 +0100

    Add updateName & updatePrice method exo4

--- src/components/Add.js
+++ src/components/Add.js
@@ -2,6 +2,29 @@ import React from "react";
 import Button from "./Button.js";
 
 class Add extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      productName: '',
+      price: 1
+    }
+
+    this.updatePrice = this.updatePrice.bind(this);
+    this.updateProductName = this.updateProductName.bind(this)
+  }
+
+  updateProductName(e) {
+    this.setState({
+      productName: e.target.value,
+    })
+  }
+
+  updatePrice(e) {
+    this.setState({
+      price: e.target.value,
+    })
+  }
+
   render() {
     return (
       <div>
@@ -21,6 +44,11 @@ class Add extends React.Component {
           children="Pay"
         />
         <h2>ADD</h2>
+        <div>
+          <input type="text" onChange={this.updateProductName}/>
+          <input type="range" min={1} max={10} onChange={this.updatePrice}/>
+          <button onClick={}>Add</button>
+        </div>
       </div>
     );
   }
